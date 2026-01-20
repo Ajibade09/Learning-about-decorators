d4b736c7a7a230da3cf5ec03e9b25b6dd643b5e5
Ajibade09 <isaabdurrahman2020@gmail.com>
2026-01-20 12:39:42 +0100

    Add simple codes demonstration the basic function to build a strong intuition about decorators

--- decorators.py
+++ decorators.py
@@ -1,1 +1,47 @@
-print(" In the decorators files")
+# #print(" In the decorators files")
+
+
+# def add(a, b): 
+#     return a + b 
+
+
+# sum = add(4, 3)
+
+
+# print("After assigning add to sum")
+# sum()
+# print("After calling sum")
+
+
+
+
+
+
+# def add(a, b): 
+#     return a + b
+
+# sum = add
+
+# print(sum(4, 3))
+
+
+
+
+# def parent():
+#     print("I am a parent")
+
+#     def child():
+#         print("I am a child")
+
+#     child()
+
+# parent()
+
+def outer():
+    def inner():
+         print("Python is cool")
+
+    return inner
+
+my_func = outer()
+my_func()
